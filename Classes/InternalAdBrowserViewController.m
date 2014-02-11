e140bfa04d1467e720cbf53b3ef80a74035890b2
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-02-11 18:18:07 -0500

    Create proper handler loadAdForURL in InternalAdBrowserViewController.

--- Classes/InternalAdBrowserViewController.m
+++ Classes/InternalAdBrowserViewController.m
@@ -11,8 +11,34 @@
 #import "AdHawkAPI.h"
 #import "AdHawkAd.h"
 #import "Settings.h"
+#import <AFNetworking.h>
+#import <AFNetworking/UIAlertView+AFNetworking.h>
+
+@interface InternalAdBrowserViewController ()
+
+@property (nonatomic, strong) AFHTTPRequestOperationManager *sessionManager;
+
+- (void)loadAdForURL:(NSURL *)url;
+
+@end
 
 @implementation InternalAdBrowserViewController
+
+@synthesize sessionManager;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    if (!self.sessionManager) {
+        self.sessionManager = [AFHTTPRequestOperationManager manager];
+        AFHTTPRequestSerializer *requestSerializer = [AFHTTPRequestSerializer serializer];
+        [requestSerializer setValue:ADHAWK_APP_USER_AGENT forHTTPHeaderField:@"X-Client-App"];
+        [requestSerializer setValue:ADHAWK_APP_USER_AGENT forHTTPHeaderField:@"User-Agent"];
+        [requestSerializer setValue:ADHAWK_APP_USER_AGENT forHTTPHeaderField:@"User_Agent"];
+        self.sessionManager.requestSerializer = requestSerializer;
+    }
+}
 
 #pragma mark - UIWebViewDelegate methods
 
@@ -21,15 +47,8 @@
     BOOL shouldStartLoad = [super webView:p_webView shouldStartLoadWithRequest:p_request navigationType:navigationType];
 
     if (navigationType == UIWebViewNavigationTypeLinkClicked) {
-        AdDetailViewController *vc = [[UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil] instantiateViewControllerWithIdentifier:@"AdDetailViewController"];
-        
-        AdHawkAd *theAd = [[AdHawkAPI sharedInstance] getAdHawkAdFromURL:[p_request URL]];
-
-        if (theAd != NULL) {
-            [p_webView stopLoading];
-            vc.targetURL = theAd.resultURL;
-            [self.navigationController pushViewController:vc animated:YES];
-        }
+        [p_webView stopLoading];
+        [self loadAdForURL:[p_request URL]];
 
         return NO;
     }
@@ -37,9 +56,26 @@
     return shouldStartLoad;
 }
 
-- (void)webView:(UIWebView *)p_webView didFailLoadWithError:(NSError *)error
+- (void)loadAdForURL:(NSURL *)url
 {
-    NSLog(@"error: %@", [error localizedDescription]);
+    NSLog(@"Requesting: %@", [url absoluteString]);
+
+    [self.activityIndicator startAnimating];
+    __weak InternalAdBrowserViewController *weakSelf = self;
+    AFHTTPRequestOperation *operation = [self.sessionManager GET:[url absoluteString] parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        AdHawkAd *ad = [[AdHawkAPI sharedInstance] convertResponseToAdHawkAd:responseObject];
+        AdDetailViewController *vc = [[UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil] instantiateViewControllerWithIdentifier:@"AdDetailViewController"];
+
+        if (ad) {
+            vc.targetURL = ad.resultURL;
+            vc.shareText = ad.shareText;
+            [weakSelf.navigationController pushViewController:vc animated:YES];
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Failure");
+    }];
+
+    [UIAlertView showAlertViewForRequestOperationWithErrorOnCompletion:operation delegate:self];
 }
 
 @end
